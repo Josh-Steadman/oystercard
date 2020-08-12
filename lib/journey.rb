e0d97c8bffd7d477c9efea8669174c88456873da
joshSteadman <josh.steadman15@gmail.com>
2020-08-12 15:24:07 +0100

    tested initialize has an entry station

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,4 +1,19 @@
 
 class Journey
 
+    def initialize
+       
+    end
+
+    def end_journey
+    
+    end
+
+    def penalty_fare
+
+    end
+
+    def journey_complete
+
+    end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,5 +1,11 @@
 require 'journey'
 describe Journey do
-
+    let(:first_station) { double :first_station }
+    let(:second_station)  { double :second_station }
+    describe '#initialize' do
+      it 'starts the journey' do
+         expect(subject.entry_station).to eq first_station
+      end
+    end
 
 end
