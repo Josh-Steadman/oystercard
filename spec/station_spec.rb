fdf94695fe7b823b9dd4a3e62b32a9541d642fe2
joshSteadman <josh.steadman15@gmail.com>
2020-08-12 14:48:59 +0100

    allowed station to be initialized with a name and zone and tested for

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -5,6 +5,9 @@ describe Station do
     it 'generates the name of the station' do
         expect(hackney.station_name).to eq 'hackney'
     end
+    it 'generates the zone of the station' do
+        expect(hackney.zone).to eq 2
+    end
   end
 
 end
