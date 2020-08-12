f036fff4d75564541cba83fa2ce5508eabf9e1b1
joshSteadman <josh.steadman15@gmail.com>
2020-08-12 15:55:39 +0100

    ending journey now returns corret fare

--- lib/journey.rb
+++ lib/journey.rb
@@ -6,12 +6,12 @@ class Journey
     end
     PENALTY_FARE = 6 
    
-    def end_journey
-    
+    def end_journey(exit_station)
+      @exit_station = exit_station
     end
 
     def fare
-      PENALTY_FARE
+     @exit_station != nil ? 1 : PENALTY_FARE
     end
 
     def journey_complete
