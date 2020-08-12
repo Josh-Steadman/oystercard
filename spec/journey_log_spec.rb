163baa2759d0c5d89d696ea8a1040bd22d7d6cec
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-12 17:15:04 +0100

    Added a failing test to check in initializes with a journey class object

--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -1,5 +1,14 @@
 require 'journey_log'
 
 describe JourneyLog do
+  let(:station) { double :station, zone: 1}
+  let(:journey) { double :journey, :station }
+  let(:journey_class) { double :journey_class}
+
+  describe '#initialize' do
+    it 'has a Journey class' do
+      expect(subject.journey_class).to be_instance_of journey
+    end
+  end
   
 end
