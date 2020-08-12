fc517c36a48e132781ea90db4e1eef8dd788c1b4
joshSteadman <josh.steadman15@gmail.com>
2020-08-12 15:29:43 +0100

    Journey now initialized with entry_station

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,8 +1,8 @@
 
 class Journey
-
-    def initialize
-       
+    attr_reader :entry_station
+    def initialize(entry_station = nil)
+       @entry_station = entry_station
     end
 
     def end_journey
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -4,7 +4,8 @@ describe Journey do
     let(:second_station)  { double :second_station }
     describe '#initialize' do
       it 'starts the journey' do
-         expect(subject.entry_station).to eq first_station
+         journey = Journey.new(first_station)
+         expect(journey.entry_station).to eq first_station
       end
     end
 
