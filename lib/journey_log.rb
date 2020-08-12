3f618897638d1a53f71a1b3a3e05ddf39f4e8926
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-12 17:26:50 +0100

    Added failing tests checking that the journey class responds to the journeylog methods

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,2 +1,6 @@
 class JourneyLog
+  attr_reader :journey_class
+  def initialize(journey_class = Journey.new)
+    @journey_class = journey_class
+  end
 end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -2,13 +2,19 @@ require 'journey_log'
 
 describe JourneyLog do
   let(:station) { double :station, zone: 1}
-  let(:journey) { double :journey, :station }
+  let(:journey) { double :journey }
   let(:journey_class) { double :journey_class}
 
   describe '#initialize' do
-    it 'has a Journey class' do
+    xit 'has a Journey class' do
       expect(subject.journey_class).to be_instance_of journey
     end
   end
+
+  it { is_expected.to respond_to start}
+  it { is_expected.to respond_to finish}
+  it { is_expected.to respond_to journeys}
   
 end
+
+
